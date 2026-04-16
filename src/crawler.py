6e2162b7148571dfa676388695cf66e670f50c16
TomO256 <github@whiskey.aleeas.com>
2026-04-16 19:38:06 +0100

    Query all text, not only quotes

--- src/crawler.py
+++ src/crawler.py
@@ -2,9 +2,33 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
+
 def crawl():
     url = "https://quotes.toscrape.com"
     content = []
+
+    content, authors = crawlQuotes(content,url)
+    content = crawlAuthors(content,authors)
+    return content
+    
+    
+def crawlAuthors(content,authors):
+    for a in authors:
+        print("Indexing: "+a)
+        response = requests.get(a)
+        if response.status_code !=200:
+            continue
+        soup = BeautifulSoup(response.text,"html.parser")
+        bio = soup.find("div", class_="author-description")
+        name = soup.find("h3", class_="author-title")
+        if bio and name:
+            final = name.text + " " + bio.text
+            content.append((a, final))
+        time.sleep(0)
+    return content
+
+def crawlQuotes(content,url):
+    authors = set()
     urlToCrawl = url
     while 1:
         print("Indexing: "+urlToCrawl)
@@ -15,7 +39,23 @@ def crawl():
         quotes = soup.find_all("span",class_="text")
         if not quotes:
             break
-        content.append((urlToCrawl,quotes))
+        for quote in quotes:
+            parent = quote.find_parent("div", class_="quote")
+            text = quote.text
+            author = parent.find("small", class_="author").text
+            tag_elements = parent.find_all("a", class_="tag")
+            tags=[]
+            for tag in tag_elements:
+                tags.append(tag.text)
+            final = text + " " + author + " " + " ".join(tags)
+            content.append((urlToCrawl, final))
+        
+        for quote in quotes:
+            parent = quote.find_parent("div", class_="quote")
+            author = parent.find("a")
+            if author:
+                link = author["href"]
+                authors.add(url + link)
         try:
             next = soup.find("li",class_="next").find("a")["href"]
         except AttributeError:
@@ -26,5 +66,5 @@ def crawl():
             print("Crawl Finished")
             break
         
-        time.sleep(6)
-    return content
+        time.sleep(0)
+    return content, authors
