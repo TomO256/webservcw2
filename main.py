80a66ecf2c58f8af2a67cb40c3468ff08bbe9df1
TomO256 <github@whiskey.aleeas.com>
2026-04-16 16:14:48 +0100

    Added find and display functionality

--- main.py
+++ main.py
@@ -55,9 +55,28 @@ def load():
         print("Unable to find index.json. Please ensure file is named correctly, or run 'build' to create it")
 
 def display(word):
-    return
+    if word not in index:
+        print("Word not found")
+        return
+    print("Index position found for "+word)
+    for page, pos in index[word].items():
+        print("Page: "+str(page)+", Position: "+str(pos))
 
 def find(string):
+    words = string.split(" ")
+    results = []
+    for i in words:
+        if i not in index:
+            print("Unable to find entry for: "+str(i))
+            return
+        results.append(set(index[i].keys()))
+    pages = set.intersection(*results)
+    if not pages:
+        print("No pages located")
+    else:
+        print("Pages containing query")
+        for i in pages:
+            print(i)
     return
 
 
